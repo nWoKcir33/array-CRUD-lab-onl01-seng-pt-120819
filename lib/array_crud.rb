1b9e2f92974f52dd3081fbf7585364ebd4b596ce
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-14 03:00:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index = ["apple", "banana", "am", "dog"]
+  retrieve_element_from_index [2]
 end
 
 def retrieve_first_element_from_array(array)
