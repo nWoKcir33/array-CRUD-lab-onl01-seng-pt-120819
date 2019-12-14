81e0e1cd80a8fc4218d29c000bc1e98336bd6e44
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-14 03:03:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,5 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array = ["apple", "banana", "cat", "arrays!"]
+  retrieve_last_element_from_array [-1]
 end
