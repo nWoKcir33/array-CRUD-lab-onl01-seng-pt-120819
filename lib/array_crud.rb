196f1b9602a12819dcafba58ddf2fffb675fa87c
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-14 02:50:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   Alphabet = ["apple", "banana", "cat"]
-  Alphabet.push(dog)
+  Alphabet.push("dog")
 end
 
 def add_element_to_start_of_array(array, element)
